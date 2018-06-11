e1f0f9c211b2b797e4f04fd1a3c0899753449400
中山弘瑛 <hiroaki1207.gk@gmail.com>
2018-06-11 22:34:58 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by 中山弘瑛 on 2018/06/11.
+//  Copyright © 2018年 中山弘瑛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
